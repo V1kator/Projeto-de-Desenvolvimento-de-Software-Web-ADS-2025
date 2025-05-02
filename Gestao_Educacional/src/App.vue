bdb942e5de473d0ed93ff295b82efff8b5508146
Vitor Hugo Sodré Quinelato <vitorhugoquinelato@gmail.com>
2025-05-02 19:21:40 -0400

    fazendo page login, sing up é atualizacao da ToList funcional

--- Gestao_Educacional/src/App.vue
+++ Gestao_Educacional/src/App.vue
@@ -1,12 +1,24 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
 
 <template>
   <div class="container-xxl position-relative bg-white d-flex p-0">
     <RouterLink to="/chamada">Sonho</RouterLink>
     <RouterView />
+    <SignIn />
   </div>
 
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Spinner from './components/Spinner.vue'
+import Login from './views/Login.vue'
+
+const loading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false
+  }, 2000)
+})
+</script>
 
